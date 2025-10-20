eeb392a7fa0a382ae448f0b36dc000144fe325bf
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-10-20 14:32:58 -0400

    Created framework of how it would work

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,20 +5,40 @@ import { useState } from "react";
 //    (after react-router-dom is setup of course)
 // 2. Create a css file to link to this page, and change the spacing between 
 //    email and password
+// 3. When adding real validation, check to see if this is how it's done 
+//    (Probably REALLY not)
+
+function btn(password, email) {
+    //Function works as expected
+    
+    //console.log(password, "--", email)
+}
 
 export default function LoginPage() {
-    
+    const [emailText, setEmailText] = useState("");
+    const [passwordText, setPasswordText] = useState("");
+
     return(<>
         <h1>Log in</h1>
 
         <label>Email: </label> 
-        <input type="text" placeholder="Enter email" />
+        <input 
+            type="text" placeholder="Enter email" 
+            onChange={e => setEmailText(e.target.value)}
+        />
         <br />
 
         <label>Password: </label> 
-        <input type="password" placeholder="Enter password" />
+        <input 
+            onChange={e => setPasswordText(e.target.value)}
+            type="password" 
+            placeholder="Enter password" 
+        />
         <br />
 
+        <button onClick={() => btn(emailText, passwordText)}>Login</button>
+        <br /> <br />
+
         <p>New user? Then click <a href="localhost">here</a></p>
-    </>)
+    </>);
 }
